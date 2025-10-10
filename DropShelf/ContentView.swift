21e0610403bf6f3f7fd48f53dfa51f6c65a0d2c5
Oleg Churkin <olchu30@yandex.ru>
2025-10-10 21:57:12 +0200

    Initial Commit

---
+++ DropShelf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DropShelf
+//
+//  Created by Oleg Churkin on 10/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DropShelf/DropShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropShelfApp.swift
+//  DropShelf
+//
+//  Created by Oleg Churkin on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DropShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
